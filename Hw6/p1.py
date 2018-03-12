7b1fc535898f6a26da3e7a59687b92ce36656f0c
regasj2018 <jregas@sjnma.org>
2018-03-11 20:29:22 -0500

    Update

--- Hw6/p1.py
+++ Hw6/p1.py
@@ -4,7 +4,7 @@ if(type == "1"):
 	score = map(int, raw_input("Enter your score frame by frame seperated by a comma").split(","))
 	print(str(sum(score)))
 if(type == "2"):
-        frame = 0
+	frame = 0
 	frame = frame + 1
         while(frame < 11):
                 print("entering row " + str(frame))
